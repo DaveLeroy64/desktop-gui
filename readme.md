0acd27065dae755c06f100311191a5e755a99536
Ben Braithwaite <benjamin200@hotmail.co.uk>
2020-08-04 17:51:27 +0100

    LT download manager completed

--- readme.md
+++ readme.md
@@ -4,14 +4,19 @@ For doing useful python things easily. Work in progress, much more to be added. 
 
 TRIGGER WARNING: POLSCRAPER scans 4chan, a website that contains highly offensive language. This module contains offensive terms that the script searches for in order to build statistical data on the use of such language. The file that contains the various words the script looks for has been deliberately excluded from this repository. The script will not function without this file. I may add it to the repo later.
 
+Current Features:
+    - Polscraper: Scrapes the board and stores all the threads/replies, with a data display that allows different data to be displayed/manipulated/compared in tables and graphs
+    - Property: Scrape different property websites for properties is user-defined areas, save to database, display data and price trends/averages in tables and graph
+    - News scraper: Collects headlines from different outlets and displays them. Can be set to run periodically (hourly updates)
+    - Multithreading: all scrapers run in separate threads to allow multitasking
+    - Downloads manager: automatically (or on button press) sorts downloads into categories/folders, and logs all moves made to a text file
+
 To do:
 
 - General
-    - All scrapers now run in separate threads and can run concurrently.
     - Refresh Thread - it does sort of work but it doesn't change the status display. Fix this.
 
 - News scraper
-    - News scraper can now be set to automatically update the news database
     - Home page display to display most recent stories in database for each outlet. 
 
 - Property scraper
@@ -20,9 +25,6 @@ To do:
 - Polscraper
     - Boolean "apply weighting" tickbox to polscraper data to weigh down 'topics' that are ridiculously overused
     - Remove datagraph.clear() statements as default whenever graph is populated - only when button is hit. This way you can overlay different data maybe?
-
-- Downloads manager
-    - Start making this...
 
 - HomeBot integration
     - Control computer remotely using telegram bot as authorised user.
--- scripts/dl_manager.py
+++ scripts/dl_manager.py
@@ -7,12 +7,13 @@ from datetime import datetime
 
 current_time = datetime.now().strftime("%Y-%m-%d %H:%M")
 
-download_folder = "C:\\Users\\Ben\\Downloads\\test"
+download_folder = "C:\\Users\\Ben\\Downloads"
 
 file_types = {
     'image': ['.jpg',
                '.jpeg',
                '.png',
+               '.jpe'
     ],
     'video': ['.mp4',
                '.wmv',
@@ -29,14 +30,21 @@ file_types = {
              '.txt',
              '.rtf',
              '.md',
-             '.pdf'],
+             '.pdf',
+             '.azw',
+             '.azw3',
+             '.epub'],
     'archive': ['.rar',
                 '.zip',
-                '.7zip'],
+                '.7zip',
+                '.7z',
+                '.iso'],
     'executable': ['.exe',
                    '.bat',
                    '.app',
-                   '.msi'],
+                   '.msi',
+                   '.bin',
+                   '.ico'],
     'presentation': ['.ppt',
                      '.pptx',
                      '.odp'],
@@ -54,13 +62,14 @@ file_destinations = {
     'archive': 'C:\\Users\\Ben\\Downloads\\Archive Files',
     'spreadsheet': 'C:\\Users\\Ben\\Documents\\Downloaded Documents\\Spreadsheets',
     'presentation': 'C:\\Users\\Ben\\Documents\\Downloaded Documents\\Presentations',
-    'executable': 'C:\\Users\\Ben\\Downloads\\Executables'
+    'executable': 'C:\\Users\\Ben\\Downloads\\Executables',
+    'unknown': 'C:\\Users\\Ben\\Downloads\\Unknown Files',
 }
 
 def log_move(file, path):
     print("log move")
     with open("C:\\Users\\Ben\\Downloads\\file_move_log.txt", "a+") as logfile:
-        logfile.write(current_time + ": " + path + " <-- " + str(file[23:]) + "\n")
+        logfile.write(current_time + ": " + str(file[23:]) + " --> " + path + "\n")
         print("logged")
 
 def move_file(file, filetype):
@@ -68,12 +77,22 @@ def move_file(file, filetype):
         if filetype == ftype:
             print("move " + file + " to " + path)
             if os.path.exists(path):
-                # shutil.move(file, path)
-                log_move(file, path)
+                try:
+                    shutil.move(file, path)
+                    log_move(file, path)
+                    return "success"
+                except:
+                    print(file + " already exists in " + path)
+                    return "failed"
             else:
-                # os.mkdir(path)
-                # shutil.move(file, path)
-                log_move(file, path)
+                os.mkdir(path)
+                try:
+                    shutil.move(file, path)
+                    log_move(file, path)
+                    return "success"
+                except:
+                    print(file + " already exists in " + path)
+                    return "failed"
 
 def file_type(file):
     print("==============")
@@ -86,23 +105,39 @@ def file_type(file):
             file_known = "yes"
             print(file)
             print("Type: " + key)
-            move_file(file, key)
+            if move_file(file, key) == "success":
+                return "moved"
                 
         
     if file_known != "yes":
-        
+        # check if the download is a directory
         if os.path.isdir(file):
             print(file)
             print("is directory")
+            
+            par1 = "("
+            par2 = ")"
+            # check if the directory is a movie directory (as they sometimes come in folders with the year in brackets)
+            if par1 in file:
+                print("FILM FOLDER")
+                year = file[file.find(par1)+1 : file.find(par2)]
+                print(year)
+                if int(year):
+                    if move_file(file, "video") == "success":
+                        return "moved"
+                           
         else:
             print(file)
             print("UNKNOWN")
+            if move_file(file, "unknown") == "success":
+                return "moved"
+    return "not moved"
     
 def sort():
     file_count = 0
     for item in os.listdir(download_folder):
         item = str(download_folder) + "\\" + str(item)
         if "file type paths" not in item and "file_move_log" not in item:
-            file_type(item)
-            file_count += 1
+            if file_type(item) == "moved": # we only want to update the count if we actually moved the file/folder
+                file_count += 1
     return file_count
